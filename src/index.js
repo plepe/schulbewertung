c7e10272779c543178449dddf15deb3d30bd011a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-02-15 09:58:40 +0100

    Read csv file and output each line to console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-const csvtojson = require("csvtojson")
+const csvtojson = require('csvtojson')
+
+const httpGet = require('./httpGet')
+
+window.onload = () => {
+  httpGet('data/schulen.csv', {}, (err, result) => {
+    if (err) {
+      return alert(err)
+    }
+
+    csvtojson()
+      .fromString(result.body)
+      .subscribe((line) => {
+        console.log(line)
+      })
+  })
+}
