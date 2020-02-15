7ab0df35756257adc51e0db2e5a08a55ad5a7164
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-02-15 10:39:10 +0100

    Add a select and echo selected option to console

--- src/index.js
+++ src/index.js
@@ -2,16 +2,33 @@ const csvtojson = require('csvtojson')
 
 const httpGet = require('./httpGet')
 
+let schulen
+
 window.onload = () => {
   httpGet('data/schulen.csv', {}, (err, result) => {
     if (err) {
       return alert(err)
     }
 
+    schulen = document.getElementById('schulen')
+
     csvtojson()
       .fromString(result.body)
       .subscribe((line) => {
-        console.log(line)
+        let option = document.createElement('option')
+        option.value = line.FID
+        option.data = line
+        option.appendChild(document.createTextNode(line.BEZIRK + ', ' + line.NAME))
+        schulen.appendChild(option)
       })
+
+    schulen.onchange = open
+    global.setTimeout(open, 1)
   })
+}
+
+function open () {
+  let option = schulen.selectedOptions[0]
+  let data = option.data
+  console.log(data)
 }
